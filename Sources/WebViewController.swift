5b0bbaa9e133323b0056d22240d5967fa906fba5
niiaua <niiaua@users.noreply.github.com>
2026-08-20 12:24:52 +0000

    把 ourlamp.cc 打包成 iOS app（ourlamp）：URL/域名/应用名/图标/标志板

--- Sources/WebViewController.swift
+++ Sources/WebViewController.swift
@@ -4,7 +4,7 @@ import WebKit
 final class WebViewController: UIViewController {
 
     // ★ 改成你的网站地址
-    private static let homeURL = URL(string: "https://example.com")!
+    private static let homeURL = URL(string: "https://ourlamp.cc")!
 
     private var webView: WKWebView!
     private var themeObservation: NSKeyValueObservation?
